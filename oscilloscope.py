0f912a782152fb3a0af57173d25dde69e6abd2a5
root <root@beaglebone.(none)>
2014-03-09 17:28:45 +0000

    made code work

--- oscilloscope.py
+++ oscilloscope.py
@@ -1,5 +1,6 @@
 import beaglebone_pru_adc as adc
 import time
+import motors
 
 def oscilloscope(capture, offset, filename, numsamples=10000):
 	
@@ -21,42 +22,42 @@ capture.start()
 print 'Capturing AIN0 at rest'
 oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_atrest.csv')
 
-with motors.motor_setup(*motors.RIGHT_MOTOR_PIN) as run:
+with motors.motor_setup(*motors.RIGHT_MOTOR_PINS) as run:
 	run(80) # run motor forward
-	time.sleep(0.5) # let it settle
+	time.sleep(2.0) # let it settle
 
 	print 'Capturing AIN0 at run (forward)'
 	oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_forward.csv')
 	
 	run(0)
-	time.sleep(0.5)
+	time.sleep(2.0)
 	
 	run(-80)
 	print 'Capturing AIN0 at run (reverse)'
 	oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_backward.csv')
 
 	run(0)
-	time.sleep(0.5)
+	time.sleep(2.0)
 	
 print 'Capturing AIN2 at rest'
 oscilloscope(capture, adc.OFF_VALUES + 8, 'data_ain2_atrest.csv')
 
-with motors.motor_setup(*motors.LEFT_MOTOR_PIN) as run:
+with motors.motor_setup(*motors.LEFT_MOTOR_PINS) as run:
 	run(80) # run motor forward
-	time.sleep(0.5) # let it settle
+	time.sleep(2.0) # let it settle
 
 	print 'Capturing AIN2 at run (forward)'
 	oscilloscope(capture, adc.OFF_VALUES+8, 'data_ain2_forward.csv')
 	
 	run(0)
-	time.sleep(0.5)
+	time.sleep(2.0)
 	
 	run(-80)
 	print 'Capturing AIN2 at run (reverse)'
 	oscilloscope(capture, adc.OFF_VALUES+8, 'data_ain2_backward.csv')
 
 	run(0)
-	time.sleep(0.5)
+	time.sleep(2.0)
 
 capture.stop()
 capture.wait()
