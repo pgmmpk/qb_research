f5bbf8eb2dbc88d80ee3927a43c4ce6550beff15
Mike Kroutikov <pgmmpk@gmail.com>
2014-03-09 13:34:48 -0400

    fixes

--- oscilloscope.py
+++ oscilloscope.py
@@ -1,9 +1,14 @@
 import beaglebone_pru_adc as adc
 import time
+import motors
 
-def oscilloscope(capture, offset, filename, numsamples=10000):
+def oscilloscope(offset, filename, numsamples=10000):
 	
+	capture = adc.Capture()
+	capture.start()
+
 	capture.oscilloscope_init(offset, numsamples)
+	capture.start()
 	
 	for _ in range(10):
 	    if capture.oscilloscope_is_complete():
@@ -14,53 +19,49 @@ def oscilloscope(capture, offset, filename, numsamples=10000):
 	with open(filename, 'w') as f:
 	    for x in capture.oscilloscope_data(numsamples):
 	        f.write(str(x) + '\n')
-
-capture = adc.Capture()
-capture.start()
+	
+	capture.stop()
+	capture.wait()
+	capture.close()
 
 print 'Capturing AIN0 at rest'
-oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_atrest.csv')
+oscilloscope(adc.OFF_VALUES, 'data_ain0_atrest.csv')
 
-with motors.motor_setup(*motors.RIGHT_MOTOR_PIN) as run:
+with motors.motor_setup(*motors.RIGHT_MOTOR_PINS) as run:
 	run(80) # run motor forward
 	time.sleep(0.5) # let it settle
 
 	print 'Capturing AIN0 at run (forward)'
-	oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_forward.csv')
+	oscilloscope(adc.OFF_VALUES, 'data_ain0_forward.csv')
 	
 	run(0)
 	time.sleep(0.5)
 	
 	run(-80)
 	print 'Capturing AIN0 at run (reverse)'
-	oscilloscope(capture, adc.OFF_VALUES, 'data_ain0_backward.csv')
+	oscilloscope(adc.OFF_VALUES, 'data_ain0_backward.csv')
 
 	run(0)
 	time.sleep(0.5)
 	
 print 'Capturing AIN2 at rest'
-oscilloscope(capture, adc.OFF_VALUES + 8, 'data_ain2_atrest.csv')
+oscilloscope(adc.OFF_VALUES + 8, 'data_ain2_atrest.csv')
 
-with motors.motor_setup(*motors.LEFT_MOTOR_PIN) as run:
+with motors.motor_setup(*motors.LEFT_MOTOR_PINS) as run:
 	run(80) # run motor forward
 	time.sleep(0.5) # let it settle
 
 	print 'Capturing AIN2 at run (forward)'
-	oscilloscope(capture, adc.OFF_VALUES+8, 'data_ain2_forward.csv')
+	oscilloscope(adc.OFF_VALUES+8, 'data_ain2_forward.csv')
 	
 	run(0)
 	time.sleep(0.5)
 	
 	run(-80)
 	print 'Capturing AIN2 at run (reverse)'
-	oscilloscope(capture, adc.OFF_VALUES+8, 'data_ain2_backward.csv')
+	oscilloscope(adc.OFF_VALUES+8, 'data_ain2_backward.csv')
 
 	run(0)
 	time.sleep(0.5)
 
-capture.stop()
-capture.wait()
-capture.close()
-
 print 'done'
-
